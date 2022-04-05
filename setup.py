a9f97015095c62b5c14a6c32942b645fe1093be3
EmmaMireyaSm <esanchez@grupoabraxas.com>
2022-04-04 23:41:36 -0500

    .

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-if __name__ == '__main__':
-
-    # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-    setup(
-        name="Amarela",
-        install_requires=[
-            "pandas >= 1.3.3",
-            "importlib-metadata >= 3.6.0; python_version < '3.10'",
-        ],
-        extras_require={
-            "dotenv": ["python-dotenv"],
-        },
-    )
+setup()
